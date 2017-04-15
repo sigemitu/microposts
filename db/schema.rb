d1cfb3cd0e2b48a48d92778fcb1b5b9bac702b2e
sigemitu <re180324-3873@tbz.t-com.ne.jp>
2017-04-15 06:34:08 +0000

    add sign sign up and user detail

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413074316) do
+ActiveRecord::Schema.define(version: 20170414023821) do
 
-  create_table "usernames", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
-
-  add_index "usernames", ["email"], name: "index_usernames_on_email", unique: true
 
 end
