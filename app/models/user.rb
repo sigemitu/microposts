59c0521a59080fc2a3e80f365ddf725b05fc3e77
sigemitu <re180324-3873@tbz.t-com.ne.jp>
2017-04-20 04:39:42 +0000

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+ uniqueness: { case_sensitive: false }
+                     validates :location, allow_blank: true, 
+                       length: { minimum: 2, maximum: 20 }
     has_secure_password
+     has_many :microposts
 end
