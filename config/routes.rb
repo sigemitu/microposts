06616212a0f36e4cb05babb690167d8460b25d29
sigemitu <re180324-3873@tbz.t-com.ne.jp>
2017-04-20 04:51:51 +0000

    fix branch_data user-profile

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
- 
- resources :microposts
- end
+  resources :users
+  resources :microposts
+end
